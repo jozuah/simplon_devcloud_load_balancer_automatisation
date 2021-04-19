2a994b8a799e2ef20307589c8e3c748d0a732e72
jozuah <joshua.galbas@gmail.com>
2021-04-19 21:54:58 +0200

    Doc azure pour set un load balancer sur 3 vm

---
+++ loadbalancer.sh
@@ -0,0 +1,78 @@
+az group create --name myResourceGroupLoadBalancer --location eastus
+
+az network public-ip create \
+    --resource-group myResourceGroupLoadBalancer \
+    --name myPublicIP
+
+az network lb create \
+    --resource-group myResourceGroupLoadBalancer \
+    --name myLoadBalancer \
+    --frontend-ip-name myFrontEndPool \
+    --backend-pool-name myBackEndPool \
+    --public-ip-address myPublicIP
+
+az network lb probe create \
+    --resource-group myResourceGroupLoadBalancer \
+    --lb-name myLoadBalancer \
+    --name myHealthProbe \
+    --protocol tcp \
+    --port 80
+
+az network lb rule create \
+    --resource-group myResourceGroupLoadBalancer \
+    --lb-name myLoadBalancer \
+    --name myLoadBalancerRule \
+    --protocol tcp \
+    --frontend-port 80 \
+    --backend-port 80 \
+    --frontend-ip-name myFrontEndPool \
+    --backend-pool-name myBackEndPool \
+    --probe-name myHealthProbe
+
+az network vnet create \
+    --resource-group myResourceGroupLoadBalancer \
+    --name myVnet \
+    --subnet-name mySubnet
+
+az network nsg create \
+    --resource-group myResourceGroupLoadBalancer \
+    --name myNetworkSecurityGroup
+
+az network nsg rule create \
+    --resource-group myResourceGroupLoadBalancer \
+    --nsg-name myNetworkSecurityGroup \
+    --name myNetworkSecurityGroupRule \
+    --priority 1001 \
+    --protocol tcp \
+    --destination-port-range 80
+
+for i in `seq 1 3`; do
+    az network nic create \
+        --resource-group myResourceGroupLoadBalancer \
+        --name myNic$i \
+        --vnet-name myVnet \
+        --subnet mySubnet \
+        --network-security-group myNetworkSecurityGroup \
+        --lb-name myLoadBalancer \
+        --lb-address-pools myBackEndPool
+done
+
+az vm availability-set create \
+    --resource-group myResourceGroupLoadBalancer \
+    --name myAvailabilitySet
+
+for i in `seq 1 3`; do
+    az vm create \
+        --resource-group myResourceGroupLoadBalancer \
+        --name myVM$i \
+        --availability-set myAvailabilitySet \
+        --nics myNic$i \
+        --image UbuntuLTS \
+        --admin-username azureuser \
+        --generate-ssh-keys \
+        --custom-data cloud-init.txt \
+        --no-wait
+done
+
+
+
